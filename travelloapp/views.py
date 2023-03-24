4719ebe540a98a4ceb209f1b29119a811f524d5b
Krishnendhukichu <krishnendhupsasidharan@gmail.com>
2023-03-24 16:48:00 +0530

    added

---
+++ travelloapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from.models import place,Team
+
+# Create your views here.
+def demo(request):
+    obj=place.objects.all()
+    obj1=Team.objects.all()
+    return render(request,'index.html',{'result':obj,'results':obj1})
